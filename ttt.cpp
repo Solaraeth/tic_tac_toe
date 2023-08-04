45047e52a749fd711812d35eff3de81628c106a3
Solaraeth <bpwandera@gmail.com>
2023-08-04 14:35:15 +0100

    Fix to do while loop
    - While loop now correctly ends when variable game_complete = true
    - Also all board positions are now playable for X
    - To-do
        - Alternate between O and X
        - Win conditions
        - invalid inputs

--- ttt.cpp
+++ ttt.cpp
@@ -5,10 +5,12 @@ std::vector<std::string> row1 = {" ", "|", " ", "|", " "};
 std::vector<std::string> row2 = {" ", "|", " ", "|", " "};
 std::vector<std::string> row3 = {" ", "|", " ", "|", " "};
 std::string divider = "-----";
-int turn =1;
+int turn = 1;
+int position;
 
 void current_board() {
-        
+
+        std::cout << "\n";
         std::cout << row1[0] << row1[1] << row1[2] << row1[3] << row1[4]<< "\n";
         std::cout << divider << "\n";
         std::cout << row2[0] << row2[1] << row2[2] << row2[3] << row2[4]<< "\n";
@@ -16,18 +18,92 @@ void current_board() {
         std::cout << row3[0] << row3[1] << row3[2] << row3[3] << row3[4]<< "\n";
     
     }
+int player_move() {
+    
+    std::cout << "\n";
+    std::cout << "Turn: " << turn << "\n\n";
+    std::cout << "Pick a row: ";
+    std::cin >> position;
+    
+    return position;
+
+}
+
 
 int main() {
     
-    int position;
+    
     bool game_complete = false;
 
     do {
-
-        std::cout << "Pick a row: ";
-        std::cin >> position;
+        
+        player_move();
 
         switch (position) {
+            case 11:
+                row1[0] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            case 12:
+                row1[2] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            case 13:
+                row1[4] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            case 21:
+                row2[0] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            case 22:
+                row2[2] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            case 23:
+                row2[4] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            case 31:
+                row3[0] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            case 32:
+                row3[2] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            case 33:
+                row3[4] = "X";
+                turn = turn + 1;
+                current_board();
+                break;
+            default:
+                std::cout << "\nInvalid move, try again: \n";
+                break;
+        
+        }
+        
+        if (row1[0] == row2[2] && row2[2] == row3[4] && turn > 4) {
+
+        game_complete = true;
+        
+        }
+
+    } while (game_complete == false); 
+
+    std::cout << "\nVICTORY!\n";
+
+    }
+
+    /*switch (position) {
         case 11:
             row1[0] = "X";
             turn = turn + 1;
@@ -46,17 +122,28 @@ int main() {
         default:
             std::cout << "Invalid move, try again: \n";
             std::cin >> position;
-            break;
+            break;*/
+
+/*if (position == 11) {
+
+            row1[0] = "X";
+            current_board();
+            turn = turn + 1;
+
         }
 
-        if (row1[0] == row2[2] && row2[2] == row3[4] && turn > 4) {
+        else if (position == 22) {
 
-        game_complete = true;
-        std::cout << "VICTORY!\n";
+            row2[2] = "X";
+            current_board();
+            turn = turn + 1;
+
         }
 
-    } while (game_complete = false);
+        else {
 
-    
+            row3[4] = "X";
+            current_board();
+            turn = turn + 1;
 
-    }
+        }*/
